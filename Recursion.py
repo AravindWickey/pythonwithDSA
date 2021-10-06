4469b95e3665c92ff1e92198e22899853b690c38
Aravind Wickey <aravind261998@gmail.com>
2021-10-06 22:12:33 +0530

    recursion pgm added.

---
+++ Recursion.py
@@ -0,0 +1,10 @@
+# fibonacci Number
+def fibonacci(n):
+    assert n>=0 and int(n) == n, 'not a right input'
+    if n in [0,1]:
+        return n
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+
+
+print(fibonacci(-3))
